578393fed2a4b7b6a5f06fed48a689faa246e6c7
MComScience <m-com3@hotmail.com>
2019-02-21 21:25:28 +0700

    config env

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,11 +3,27 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
+            'dsn' => getenv('DB_DSN'),
+            'username' => getenv('DB_USERNAME'),
+            'password' => getenv('DB_PASSWORD'),
+            'tablePrefix' => getenv('DB_TABLE_PREFIX'),
+            'charset' => getenv('DB_CHARSET', 'utf8'),
+            'enableSchemaCache' => true,
+        ],
+        /*
+        'db' => [
+            'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=que-ud-municipality-h;port=3307',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
             'enableSchemaCache' => true,
+        ],*/
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => 'localhost',
+            'port' => 6379,
+            'database' => 0,
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
