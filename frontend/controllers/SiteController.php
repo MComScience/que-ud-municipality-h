aada9f97663cbf687172327b69257966f1e9a72f
docker_remaker <docker.remaker@gmail.com>
2020-09-16 01:38:50 +0700

    update satis role

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -33,7 +33,7 @@ class SiteController extends Controller
         return [
             'access' => [
                 'class' => AccessControl::className(),
-                'only' => ['logout', 'signup'],
+                'only' => ['logout', 'signup','satis'],
                 'rules' => [
                     [
                         'actions' => ['signup'],
